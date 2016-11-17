a915b819461fc807ed943e94dfb8e5ccb1dfc62d
Ronaiza Card <ronaizacard@gmail.com>
2016-11-17 21:31:30 -0200

    make mock to test

---
+++ mock.js
@@ -0,0 +1,3 @@
+const mock = '<https://api.github.com/user/1561347/starred?per_page=50&page=3>; rel="next", <https://api.github.com/user/1561347/starred?per_page=50&page=15>; rel="last", <https://api.github.com/user/1561347/starred?per_page=50&page=1>; rel="first", <https://api.github.com/user/1561347/starred?per_page=50&page=1>; rel="prev"';
+
+module.exports = mock;
